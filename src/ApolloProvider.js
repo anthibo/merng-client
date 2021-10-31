bb9999909a6392f2f525a4492e5ccc63688fc713
Abdulrahman Khalid <47614266+anthibo@users.noreply.github.com>
2021-10-31 17:57:10 +0200

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { setContext } from 'apollo-link-context'
 // import { ApolloProvider } from '@apollo/react-hooks';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://grim-labyrinth-47560.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
